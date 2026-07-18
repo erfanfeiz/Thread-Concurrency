118586a0dd52810f99eb08563c8d98ba0c570f45
a.feiz <amirfeiz7@gmail.com>
2026-07-18 11:02:52 +0330

    updated

---
+++ ThreadAndConcurrency/src/main/java/ir/pepco/tpgArian/exercises/BankTransactionProcessor.java
@@ -0,0 +1,25 @@
+package ir.pepco.tpgArian.exercises;
+
+public class BankTransactionProcessor implements Runnable {
+    private String transactionId;
+
+    public BankTransactionProcessor(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Processing transaction: " + transactionId + " - from " + Thread.currentThread().getName());
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new BankTransactionProcessor("123123"), "Txn-Processor-1");
+        Thread thread2 = new Thread(new BankTransactionProcessor("147741"), "Txn-Processor-2");
+        Thread thread3 = new Thread(new BankTransactionProcessor("258852"), "Txn-Processor-3");
+        Thread thread4 = new Thread(new BankTransactionProcessor("369963"), "Txn-Processor-4");
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
+    }
+}
