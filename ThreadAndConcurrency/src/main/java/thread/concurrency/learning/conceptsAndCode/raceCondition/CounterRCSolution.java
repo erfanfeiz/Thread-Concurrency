5fe6f336cd9c3fd546e030b62c6273d62d586477
a.feiz <amirfeiz7@gmail.com>
2026-08-11 16:29:42 +0330

    refactored

---
+++ ThreadAndConcurrency/src/main/java/thread/concurrency/learning/conceptsAndCode/raceCondition/CounterRCSolution.java
@@ -0,0 +1,34 @@
+package thread.concurrency.learning.conceptsAndCode.raceCondition;
+
+public class CounterRCSolution {
+    private static int counter = 0;
+
+    /**
+     * proper usage of synchronization
+     * @param args
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Runnable job = () -> {
+            for (int i = 0; i < 10000; i++)
+                synchronized (CounterRCSolution.class) {
+                    increment(); // Race condition here
+                }
+        };
+        Thread t1 = new Thread(job, "executer-1");
+        Thread t2 = new Thread(job, "executer-2");
+        Thread t3 = new Thread(job, "executer-3");
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
+        System.out.println("Final counter value: " + counter);
+        // Expected: 20000, Actual: Usually less (14237, 13137)
+    }
+
+    private static void increment() {
+        counter++;
+    }
+}
---
+++ ThreadAndConcurrency/src/main/java/thread/concurrency/learning/conceptsAndCode/visibility/VolatileDemo.java
@@ -0,0 +1,19 @@
+package thread.concurrency.learning.conceptsAndCode.visibility;
+
+public class VolatileDemo {
+    private static  boolean running = true; //Try removing volatile
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            System.out.println("Worker started");
+            while (running) {
+                // do work
+            }
+            System.out.println("Worker stopped");
+        }, "Worker");
+        thread.start();
+        Thread.sleep(1000);
+        System.out.println("Main thread setting running = false");
+        running = false;
+    }
+}
